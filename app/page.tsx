03a0c63341ebfb7a8eb27231c564664cec941e26
PatrickPau <patrickpauchiho@gmail.com>
2024-05-25 19:14:33 +1000

    change btn path on app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Logo from "../assets/logo.svg";
 import LandingImg from "../assets/main.svg";
-import { Button } from "@/components/ui/Button";
+import { Button } from "../components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
